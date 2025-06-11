1f28714e62571476bb8de0e571ee87263eaf4841
javi-lopez02 <mjlphone0210@gmail.com>
2025-06-11 18:00:36 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
-import users from "./Routes/user.routes"
+import users from "./Routes/user.routes";
 
 dotenv.config();
 
@@ -15,8 +15,10 @@ app.use(
       "http://localhost:4173",
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://reynol-frontend-m7adbx3v4-mjlopezs-projects.vercel.app/",
-      "http://reynol-frontend-m7adbx3v4-mjlopezs-projects.vercel.app/"
+      "https://reynol-frontend-m7adbx3v4-mjlopezs-projects.vercel.app",
+      "http://reynol-frontend-m7adbx3v4-mjlopezs-projects.vercel.app",
+      "https://reynol-frontend.vercel.app",
+      "http://reynol-frontend.vercel.app",
     ],
     credentials: true,
   })
